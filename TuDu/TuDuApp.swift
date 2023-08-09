ec0242450ffe7b67a905d306932106a9433d9a22
nghia203 <nghiamkqn@gmail.com>
2023-08-09 23:39:42 +0700

    Initial Commit

---
+++ TuDu/TuDuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuDuApp.swift
+//  TuDu
+//
+//  Created by Lê Mai Khanh on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TuDuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
